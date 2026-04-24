1cbf431cf1914ebc77a1e5554b2732fa58903db6
goche <angelfinix99@gmail.com>
2026-04-24 12:44:50 +0100

    feat: implement request logging and cleanup profile JSON

--- main.go
+++ main.go
@@ -6,19 +6,20 @@ import (
 	"fmt"
 	"net/http"
 	"strings"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
 type album struct {
-	ID     string  `json:"id"` // Removed the space after the colon
+	ID     string  `json:"id"`
 	Title  string  `json:"title"`
 	Artist string  `json:"artist"`
 	Price  float64 `json:"price"`
 }
 
 var albums = []album{
-	{ID: "1", Title: "Beast of the Hidden Leaf", Artist: "Juice World", Price: 55.80}, // Added commas
+	{ID: "1", Title: "Beast of the Hidden Leaf", Artist: "Juice World", Price: 55.80},
 	{ID: "2", Title: "The Dark Knight", Artist: "Batman", Price: 90.45},
 	{ID: "3", Title: "The Kryptonian", Artist: "Superman", Price: 80.35},
 	{ID: "4", Title: "The Honoured One", Artist: "Gojo Satoru", Price: 500.80},
@@ -26,34 +27,53 @@ var albums = []album{
 
 func main() {
 	router := gin.Default()
+
+	// Routes
 	router.GET("/albums", getAlbums)
+	router.GET("/albums/:id", getAlbumByID) // NEW: Search for one album
 	router.GET("/profile", getimages)
-	router.Run("localhost:8080") // Removed the space in the address
+
+	router.Run("localhost:8080")
 }
 
-func getAlbums(c *gin.Context) { // Capitalized Context
-	c.IndentedJSON(http.StatusOK, albums) // Passing the slice 'albums', not the type 'album'
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
+}
+
+func getAlbumByID(c *gin.Context) {
+	id := c.Param("id")
+
+	for _, a := range albums {
+		if a.ID == id {
+			c.JSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.JSON(http.StatusNotFound, gin.H{"message": "album not found"})
 }
 
 func getimages(c *gin.Context) {
-
-	email := c.Query("Email")
+	email := c.Query("email")
 	email = strings.ToLower(strings.TrimSpace(email))
+
 	if email == "" {
 		c.JSON(http.StatusBadRequest, gin.H{
-			"Error:": "You Must Provide An Email Address In The Url",
+			"error": "email query parameter is required",
 		})
 		return
 	}
+	fmt.Printf("[LOG] %s: New request for avatar: %s\n", time.Now().Format("2006-01-02"), email)
 	hash := md5.Sum([]byte(email))
 	hashstr := hex.EncodeToString(hash[:])
+
 	gravataURL := fmt.Sprintf("https://www.gravatar.com/avatar/%s?s=200", hashstr)
 	dicebearURL := fmt.Sprintf("https://api.dicebear.com/9.x/bottts/svg?seed=%s", email)
 
 	c.JSON(http.StatusOK, gin.H{
-		"email":              email,
-		"Human Avatar":       gravataURL,
-		"Robot_Avatar":       dicebearURL,
-		"user_input_message": "Choose Wisely Which You Want Your Favourite Avatar Style!",
+		"email":        email,
+		"human_avatar": gravataURL,
+		"robot_avatar": dicebearURL,
+		"developer":    "Finix",
+		"github":       "https://github.com/angelgabriel-dotcom/go-avatar-api",
 	})
 }
